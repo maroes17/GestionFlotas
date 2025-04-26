44cd9262829b6f9e435bfab7a4729800d69e5901
Matias R <maroes17@gmail.com>
2025-04-26 13:09:27 -0400

    DataTable por cada hoja de Google Sheet(lectura)

--- funciones.js
+++ funciones.js
@@ -8,12 +8,10 @@ function obtenerDatosHtml(nombre){
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
 
-function getData(){
+function getData(sheetName){
     const ss = SpreadsheetApp.openById("1RqztNinU7VGagy8P2wrTLqtXsXLr1W-Dlo_72c5OxiY");
-    const sheet = ss.getSheetByName('Flota');
+    const sheet = ss.getSheetByName(sheetName);  // Tomamos el nombre de la hoja
     const [headers, ...datos] = sheet.getDataRange().getDisplayValues();
 
-    // console.log({headers,datos});
-
-    return {headers,datos};
+    return {headers, datos}; // Retornamos los datos de la hoja
 }
